afe41b1024dc0a12f0c9c99a21a1af30f31b521c
Nikita Hrytsai <cookies.in.chocolate@gmail.com>
2025-02-17 15:09:38 +0200

    specify language

--- node/predictions.ts
+++ node/predictions.ts
@@ -15,8 +15,8 @@ const mistral = new Mistral({
 if (!key) {
   throw new Error("Mistral API key not found");
 }
-const systemMessage  = `predict what user whant to cahnge. do not explain final answer. show only code.`
-
+//TOOD we need filetype fo this
+const systemMessage = `predict what user whant to cahnge. do not explain final answer. you are code completion assistant.`;
 
 // const deleteCursor = (str: string) => str.replace(/<\|user_cursor_is_here\|>/g, "");
 const user_cusor_is_here = "<|user_cursor_is_here|>";
@@ -25,13 +25,9 @@ const editable_region_end = "<|editable_region_end|>";
 
 export async function startPredictions(nvim: Nvim) {
   const buffer = await getCurrentBuffer(nvim);
+  const filePath = await buffer.getName();
+  const fileName = langFromFileName(filePath, nvim);
   const win = await getCurrentWindow(nvim);
-  let i = 0;
-  // while(true) {
-  i++;
-  if (i === 3) {
-    return;
-  }
   // await new Promise((resolve) => setTimeout(resolve, 2000));
   const { row } = await win.getCursor();
 
@@ -41,12 +37,13 @@ export async function startPredictions(nvim: Nvim) {
 
   const relativeRow = row - start;
   const lines = await buffer.getLines({ start, end });
+  //maybe we should put all file into context but editabel region should be small?
   const withCursor = [
     editable_region_start,
     ...lines.slice(0, relativeRow),
     user_cusor_is_here,
-    editable_region_end,
     ...lines.slice(relativeRow),
+    editable_region_end,
   ];
   const prompt = withCursor.join("\n");
 
@@ -58,13 +55,28 @@ export async function startPredictions(nvim: Nvim) {
     // model: "mistral-small-latest",
     stream: false,
     maxTokens: 1000,
-		temperature: 0,
+    temperature: 0,
     messages: [
-			{ content: systemMessage, role: "system", },
-			{ content: "do not edit code before <|editable_region_start|>", role: "user", },
-			{ content: "do not edit code past <|editable_region_end|>", role: "user", },
-			{ content: prompt, role: "user", },
+      { content: systemMessage, role: "system" },
+      // {
+      //   content: "put code inside <|editable_region_start|>",
+      //   role: "user",
+      // },
+      // {
+      //   content: "do not edit outside <|editable_region_end|>",
+      //   role: "user",
+      // },
+      {
+        content: "do not edit code before <|editable_region_start|>",
+        role: "user",
+      },
+      {
+        content: "do not edit code past <|editable_region_end|>",
+        role: "user",
+      },
+      // {content: `file name is ${fileName}`, role: "user", },
 
+      { content: prompt, role: "user" },
     ],
   });
   const content = result.choices?.[0].message.content as string;
@@ -72,18 +84,25 @@ export async function startPredictions(nvim: Nvim) {
     return;
   }
   const insertLines = content.split("\n");
-  const last = insertLines.pop();
-  const first = insertLines.shift();
-  const diffStr = getColoredDiff(lines.join("\n"), insertLines.join("\n"));
+  const diffStr = getColoredDiff(
+    lines.join("\n"),
+    insertLines.join("\n"),
+    "line",
+  );
   // const text = insertLines.join("\n");
+  insertLines.pop(); //remove markdown tags which mistral always use
+  insertLines.shift(); //remove markdown tags which mistral always use
   await buffer.setLines({ start, end, lines: insertLines as Line[] });
   const endTime = performance.now();
   const elapsedTime = Math.round(endTime - startTime);
   writeDebugPredictions(
-    "prediction",
-    `${elapsedTime}\n${first}\n${diffStr}\n${last}\n`,
+    `== Prediction in file ${fileName} (${elapsedTime}ms)`,
+    `${diffStr}\n`,
   );
-  // }
+  // writeDebugPredictions(
+  //   `== Prediction in file ${fileName} (${elapsedTime}ms)`,
+  //   `${content}\n`,
+  // );
 }
 
 // function boldTextMd(text: string) {
@@ -112,7 +131,11 @@ export async function startPredictions(nvim: Nvim) {
 
 //getColoredDiff returns a string with diff lines colored.
 //It's intended for a dispaying in terminal for debug.
-function getColoredDiff(prompt: string, prompt2: string): string {
+function getColoredDiff(
+  prompt: string,
+  prompt2: string,
+  style: "line" | "word",
+): string {
   const p = prompt;
   //diff by chars gives unreadable results: Point -> Dot = PDoint
   // const d = diff.diffWords(p, prompt2);
@@ -123,7 +146,7 @@ function getColoredDiff(prompt: string, prompt2: string): string {
     const { added, removed, value } = part;
 
     // result.push(transformDiffPpart(value, 'line', added, removed));
-    result.push(transformDiffPpart(value, "line", added, removed));
+    result.push(transformDiffPpart(value, style, added, removed));
   }
 
   return result.join("");
@@ -131,22 +154,21 @@ function getColoredDiff(prompt: string, prompt2: string): string {
 
 function transformDiffPpart(
   part: string,
-  kind: "line" | "char" | "word",
+  style: "line" | "char" | "word",
   added: boolean,
   removed: boolean,
 ): string {
   if (!added && !removed) return part;
   if (added && !removed) return colorText(part, "green");
-  if (removed && !added) return colorText(strikeThrough(part), 'red');
+  if (removed && !added) return colorText(strikeThrough(part), "red");
   //dont know if diff uses this
   if (removed && added) return colorText(part, "yellow");
-  return kind === "line" ? part + "\n" : part;
+  return style === "line" ? part + "\n" : part;
 }
 
 function strikeThrough(text: string) {
-	return `\x1b[9m${text}\x1b[0m`;
+  return `\x1b[9m${text}\x1b[0m`;
 }
-
 
 function colorText(text: string, color: "green" | "red" | "yellow") {
   switch (color) {
@@ -165,4 +187,43 @@ function writeDebugPredictions(title: string, text: string) {
   fs.writeSync(file, `${title}\n`);
   fs.writeSync(file, text);
   fs.writeSync(file, "\n");
+}
+
+//TODO: its a temporary hack of course (as evrything here)
+function langFromFileName(path: string, nvim: Nvim) {
+  const index = path.lastIndexOf(".");
+  if (index === -1) {
+    nvim.logger?.error(
+      "Could not find extension in file name for code prediction",
+    );
+    return "";
+  }
+  const extension = path.slice(index + 1);
+  switch (extension) {
+    case "ts":
+      return "typescript";
+    case "js":
+      return "javascript";
+    case "jsx":
+      return "javascript react";
+    case "tsx":
+      return "typescript react";
+    case "py":
+      return "python";
+    case "lua":
+      return "lua";
+    case "rs":
+      return "rust";
+    case "json":
+      return "json";
+    case "html":
+      return "html";
+    case "css":
+      return "css";
+    case "toml":
+      return "toml";
+    default:
+      nvim.logger?.error("Unknown extension for code prediction: " + extension);
+      return "";
+  }
 }
